c4f25a9c25e1133437609985adeab42f46824f3a
Phill <s3607153@student.rmit.edu.au>
2019-09-03 17:04:30 +1000

    setup

---
+++ myTime/myTime/ViewModel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myTime
+//
+//  Created by Phillip Guettaf on 3/9/19.
+//  Copyright © 2019 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
