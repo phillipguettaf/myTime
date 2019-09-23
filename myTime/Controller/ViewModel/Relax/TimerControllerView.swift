018c413c297845fb3168d4e06b7f52b24b7cdfd4
Phill <s3607153@student.rmit.edu.au>
2019-09-23 16:58:36 +1000

    Animations set and working

--- myTime/Controller/ViewModel/Relax/TimerControllerView.swift
+++ myTime/Controller/ViewModel/Relax/TimerControllerView.swift
@@ -12,90 +12,140 @@ import UIKit
 
 class TimerControllerView: UIViewController {
     
-    @IBOutlet weak var labol: UILabel!
+    var countdownLayer = CAShapeLayer()
+    var countdownTrackLayer = CAShapeLayer()
+    var circleLayer = CAShapeLayer()
+    var duration: TimeInterval = 10
+    var timer = Timer()
+    var center:CGPoint?
+    var tapped = false {
+        didSet {
+            if tapped {
+                navbar.isHidden = false
+                countdownLayer.opacity = 1
+                countdownTrackLayer.opacity = 1
+                
+            } else {
+                navbar.isHidden = true
+                countdownLayer.opacity = 0
+                countdownTrackLayer.opacity = 0
+            }
+        }
+    }
     
-    // Draw a circle
-    let shapeLayer = CAShapeLayer()
-
+    let darkColor = UIColor(red: 33/255, green: 79/255, blue: 81/255, alpha: 1.0).cgColor
+    
+    @IBOutlet weak var navbar: UINavigationBar!
+    
+    @IBAction func onDoubleTap(_ sender: Any?) {
+        tapped.toggle()
+        self.view.layoutIfNeeded()
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let center = view.center
+        navbar.setBackgroundImage(UIImage(),for: .default)
+        navbar.shadowImage = UIImage()
+        navbar.isTranslucent = true
+        navbar.isHidden = true
+        
+        drawCircle(shapeLayer: circleLayer, color: self.darkColor)
+        animateCircle(shapeLayer: circleLayer)
+        
+        drawCountdownPaths(color: self.darkColor)
+        animateCountdown(shapeLayer: countdownLayer, duration: duration)
+        
+        countdownTrackLayer.opacity = 0
+        countdownLayer.opacity = 0
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+            // this is the code that the timer runs every second
+            (_:Timer)->Void in //  the Timer object is passed in, but we ignore it
+            if(self.duration > 0) {
+                self.duration -= 1 ; // decrement the duration
+            } else {
+                self.timer.invalidate()
+            }
+            //self.label.text = String(self.duration) // set label's text to be the remaining time
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        drawCircle(shapeLayer: circleLayer, color: self.darkColor)
+        animateCircle(shapeLayer: circleLayer)
+        
+        navbar.isHidden = false
+        drawCountdownPaths(color: self.darkColor)
+        animateCountdown(shapeLayer: countdownLayer, duration: duration)
+    }
+    
+    func drawCountdownPaths(color: CGColor) {
+        center = view.center
+        drawCountdownPath(shapeLayer: countdownTrackLayer, color: color, toBeAnimated: false)
+        drawCountdownPath(shapeLayer: countdownLayer, color: UIColor.gray.cgColor, toBeAnimated: true)
+    }
+
+    private func drawCountdownPath(shapeLayer: CAShapeLayer, color: CGColor, toBeAnimated: Bool) {
         
         // attributes of the circle
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let circularPath = UIBezierPath(arcCenter: center!, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
         shapeLayer.path = circularPath.cgPath
-        
         // Elements of the circle
-        shapeLayer.strokeColor = UIColor.black.cgColor
-        shapeLayer.lineWidth = 10
+        shapeLayer.strokeColor = color
+        shapeLayer.lineWidth = 5
+        shapeLayer.lineCap = kCALineCapRound
         shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.strokeEnd = 0
-        
+        if toBeAnimated {
+            shapeLayer.strokeEnd = 0
+        }
         view.layer.addSublayer(shapeLayer)
+    }
+    
+    private func animateCountdown(shapeLayer: CAShapeLayer, duration: TimeInterval) {
         
-        // thing we want to animate on the shape layer
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
+    
         basicAnimation.toValue = 1
-        
-        // the speed of the animation till completion. DOES NOT EQUAL SECONDS
-//        basicAnimation.duration = 30
-        basicAnimation.duration = 38
-        
+        basicAnimation.duration = 40
         // Allows the edge the stay when finished
         basicAnimation.fillMode = kCAFillModeForwards
         basicAnimation.isRemovedOnCompletion = false
         
-        shapeLayer.add(basicAnimation, forKey: " ")
-        
-        
-        var score = 30
-        let label = 30
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true)
-        {
-            // this is the code that the timer runs every second
-            (_:Timer)->Void in //  the Timer object is passed in, but we ignore it
-            if(score>0)
-            {
-                score -= 1 ; // increment the score
-            }
-            self.labol.text = String(score) // set label's text to be the current store
-        }
-
+        shapeLayer.add(basicAnimation, forKey: "countdown")
         
     }
     
+    private func drawCircle(shapeLayer: CAShapeLayer, color: CGColor) {
+        center = view.center
+        let circularPath = UIBezierPath(arcCenter: center!, radius: 25, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        // Elements of the circle
+        shapeLayer.frame = view.bounds
+        shapeLayer.strokeColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 5
+        shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.fillColor = color
+       
+        view.layer.addSublayer(shapeLayer)
+    }
     
+    private func animateCircle(shapeLayer: CAShapeLayer) {
+        let basicAnimation = CABasicAnimation(keyPath: "transform.scale")
+        
+        basicAnimation.toValue = 4
+        basicAnimation.duration = 7
+        basicAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        basicAnimation.autoreverses = true
+        basicAnimation.repeatCount = Float.infinity
+        
+        shapeLayer.add(basicAnimation, forKey: "breathe")
+        
+    }
 }
-
-// Counting Down
-
-//import Foundation
-//import Charts
-//import UIKit
-//
-//class TimerControllerView: UIViewController {
-//
-//    @IBOutlet weak var pickerView: UIDatePicker!
-//    @IBOutlet weak var lbl: UILabel!
-//    @IBOutlet weak var start: UIButton!
-//
-//    var time = 50
-//
-//    //timer
-//    var timer = Timer()
-//
-//     var pickerData: [String] = [String]()
-//
-//
-//    @IBAction func start(_ sender: UIButton)
-//    {
-//        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerControllerView.action), userInfo: nil, repeats: true)
-//    }
-//
-//    @objc func action()
-//    {
-//        time -= 1
-//        lbl.text = String(time)
-//    }
